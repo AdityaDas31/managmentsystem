6c17bba59efd9d1c12c8362451ba32c24140c790
Aditya <adityadasofficial31@gmail.com>
2022-12-02 18:39:55 +0530

    login page

--- index.php
+++ index.php
@@ -1,49 +1,74 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Resorce/userlogin.css">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    
     <link rel="icon" href="Resorce/logo.png" type="image/x-icon">
     <title>UserLogin</title>
-</head>
-<body>
-    <div class="hero">
-        <div class="form-box">
-            <div class="button-box">
-                <div id="btn"></div>
-                <button type="button" class="toggle-btn" onclick="teacher()">Teacher</button>
-                <button type="button" class="toggle-btn" onclick="student()">Student</button>
-            </div>
-            <form action="Teacher/teacherinsart.php" method="POST" id="teacher" class="input-group">
-                <input type="text" class="input-find" name="uid" placeholder="User ID" required>
-                <input type="password" class="input-find" name="password" placeholder="Password" required>
-                <button type="submit" name="submit" class="submit-btn">Login</button>
-            </form>
-            <form action="Student/studentinsart.php" method="POST" id="student" class="input-group">
-                <input type="text" class="input-find" name="uid" placeholder="User ID" required>
-                <input type="password" class="input-find" name="password" placeholder="Password" required>
-                <button type="submit" name="submit" class="submit-btn">Login</button>
-            </form>
-        </div>
+
+    <meta name="description" content="" />
+
+    <link rel="stylesheet" href="Resorce/global.css" />
+    <link rel="stylesheet" href="Resorce/index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap"
+    />
+  </head>
+  <body>
+    <div class="frame-div">
+      <img
+        class="pexels-miguel-padrin-25537-icon"
+        alt=""
+        src="Resorce/public/pexelsmiguelpadrin255379-1@2x.png"
+      />
+      <div class="logo-div">
+        <img class="logo-1-icon" alt="" src="Resorce/public/logo-1@2x.png" />
+        <p class="oliva-school-p">Oliva School</p>
+      </div>
+      
     </div>
+     
+      <div class="form-box">
+          <div class="button-box">
+              <div id="btn"></div>
+              <button type="button" class="toggle-btn" onclick="teacher()">Teacher</button>
+              <button type="button" class="toggle-btn" onclick="student()">Student</button>
+          </div>
+          <form action="Teacher/teacherinsart.php" method="POST" id="teacher" class="input-group">
+              <h2>Login In To Your Account</h2>
+              <h3>Teacher Login</h3>
+              <input type="text" class="input-find" name="uid" placeholder="User ID" required>
+              <input type="password" class="input-find" name="password" placeholder="Password" required>
+              <button type="submit" name="submit" class="submit-btn">Login</button>
+          </form>
+          <form action="Student/studentinsart.php" method="POST" id="student" class="input-group">
+              <h2>Login In To Your Account</h2>
+              <h3>Student Login</h3>
+              <input type="text" class="input-find" name="uid" placeholder="User ID" required>
+              <input type="password" class="input-find" name="password" placeholder="Password" required>
+              <button type="submit" name="submit" class="submit-btn">Login</button>
+          </form>
+      </div>
 
-    <script>
-        var x = document.getElementById("teacher");
-        var y = document.getElementById("student");
-        var z = document.getElementById("btn");
+  <script>
+    var x = document.getElementById("teacher");
+    var y = document.getElementById("student");
+    var z = document.getElementById("btn");
 
-        function student(){
-            x.style.left = "-400px";
-            y.style.left = "50px";
-            z.style.left = "110px";
-        }
-        function teacher(){
-            x.style.left = "50px";
-            y.style.left = "450px";
-            z.style.left = "0";
-        }
+    function student(){
+        x.style.left = "-400px";
+        y.style.left = "50px";
+        z.style.left = "110px";
+    }
+    function teacher(){
+        x.style.left = "50px";
+        y.style.left = "450px";
+        z.style.left = "0";
+    }
 
-    </script>
-</body>
+</script>
+  </body>
 </html>
